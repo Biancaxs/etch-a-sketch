9a5e85f72fec095bb07cb8e5a80e66c333fdebb2
Bianca Xavier <202794770+Biancaxs@users.noreply.github.com>
2026-07-03 14:27:31 -0300

    add grid resizing and fix board layout alignment

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
 const container = document.querySelector('#container')
+const button = document.querySelector('.btn')
 
 
 function createGrid(n){
+
+    container.innerHTML = ''
+
     for(let row = 0; row < n; row++){
+
         for(let column = 0; column < n; column++){
             let div = document.createElement("div")
             div.classList.add("grid")
-            // div.style.background="pink"
-            // div.style.width=`${960/n}px`
-            // div.style.height=`${960/n}px`
+
+            const size = 100 / n;
+
+            div.style.flex = `0 0 ${size}%`;
+            div.style.height = `${size}%`;
+
+
+
+            div.addEventListener("mouseover", function (e) {
+                const changeColor = e.target    
+                
+                if (changeColor.classList.contains('grid')) {
+        
+                changeColor.classList.add('change-color');
+        
+                }
+            })
+
             container.appendChild(div)
         }
     }
 }
+
+button.addEventListener("click", () => {
+    const numberOsSquares = prompt("Please enter the number of squares per side for a new grid")
+
+    if (numberOsSquares > 0 && numberOsSquares <= 100){
+        createGrid(numberOsSquares)
+    } else {
+        alert("Choose a number between 1-100")
+    }
+    
+})
 
 createGrid(16)
